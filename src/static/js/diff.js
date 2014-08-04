d2931140cabcceb4d3a42ed7efb5a62626e31a42
mcorrand <matt.corrand@gmail.com>
2014-08-03 22:20:52 -0400

    now in v2

--- src/static/js/diff.js
+++ src/static/js/diff.js
@@ -1,6 +1,9 @@
-angular.module('appv1', [])
-    .controller('appv1controller', function($scope, $http) {
+angular.module('appv2', [])
+    .controller('appv2controller', function($scope, $http) {
         $http.jsonp('http://rate-exchange.appspot.com/currency?from=EUR&to=USD&callback=JSON_CALLBACK').success(function(data){
             $scope.rate = data.rate;
+        });
+        $scope.$watch('euros', function(value){
+        	$scope.dollars = parseFloat(value) * $scope.rate;
         });
     });
