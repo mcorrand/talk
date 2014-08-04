3ad4df02387fd76f700e55b8dc8328db5f57e070
mcorrand <matt.corrand@gmail.com>
2014-08-03 22:21:16 -0400

    now in v4

--- src/static/js/diff.js
+++ src/static/js/diff.js
@@ -1,21 +1,21 @@
-angular.module('appv3', [])
-    .controller('appv3controller', function($scope, $http) {
-        var pound = {name: 'sterling pound', acronym: 'GBP', symbol: '£'};
-        var dollar = {name: 'dollar', acronym: 'USD', symbol: '$'};
-        var euro = {name: 'euro', acronym: 'EUR', symbol: '€'};
-        $scope.currencies = [pound, dollar, euro];
+angular.module('appv4', ['appv4.directives'])
+    .controller('appv4controller', function($scope, $http) {
+    	var pound = {name: 'sterling pound', acronym: 'GBP', symbol: '£'};
+    	var dollar = {name: 'dollar', acronym: 'USD', symbol: '$'};
+    	var euro = {name: 'euro', acronym: 'EUR', symbol: '€'};
+    	$scope.currencies = [pound, dollar, euro];
 
-        $scope.from = euro;
-        $scope.to = pound;
-        $scope.in = 1;
+    	$scope.from = euro;
+    	$scope.to = pound;
+    	$scope.in = 1;
 
-        $scope.$watchGroup(['from', 'to'], function(group){
-            $http.jsonp('http://rate-exchange.appspot.com/currency?from='+ group[0].acronym + '&to=' + group[1].acronym + '&callback=JSON_CALLBACK').success(function(data){
-                $scope.rate = data.rate;
-            });
-        });
+    	$scope.$watchGroup(['from', 'to'], function(group){
+	    	$http.jsonp('http://rate-exchange.appspot.com/currency?from='+ group[0].acronym + '&to=' + group[1].acronym + '&callback=JSON_CALLBACK').success(function(data){
+	            $scope.rate = data.rate;
+	        });
+    	});
 
         $scope.$watchGroup(['in', 'rate'], function(group){
-            $scope.out = parseFloat(group[0]) * group[1];
+        	$scope.out = parseFloat(group[0]) * group[1];
         });
     });
